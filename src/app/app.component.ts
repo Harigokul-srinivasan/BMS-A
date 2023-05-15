741a796eb338e403c0a91dbb6bcc29fc096c2aba
Hari Gokul <ceeeharigokul23@gmail.com>
2023-05-15 13:34:38 +0530

    Hari Gokul

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
 import { AuthService } from './auth.service';
+import { RegisterValidateService } from './registerValidate.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template:`
+<ng-container *ngIf="user">
+      <p>Name: {{ detail.firstname }}</p>
+    </ng-container>
+  `
 })
 export class AppComponent {
   title = 'bikeA';
   usersuccess=false;
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private service: RegisterValidateService) {
     this.usersuccess=Boolean(sessionStorage.getItem("usersuccess" ))||this.authService.userlogin
   }
   logout(){
     this.authService.userlogin=false;
     this.usersuccess=false;
     sessionStorage.clear();
+  }
+  detail:any;
+  ngOnInit(){
+     this.fetchdata();
+  }
+  fetchdata(){
+    this.service.getusername().subscribe(res =>{
+      this.detail=res;
+  }
+    );
   }
   }
 
--- src/app/registerValidate.service.ts
+++ src/app/registerValidate.service.ts
@@ -1,13 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegisterValidateService {
-
+   private url="http://localhost:3000/users";
   constructor(private client:HttpClient) { }
   addUserInfo(data:any){
     return this.client.post("http://localhost:3000/users",data);
+  }
+  getusername(): Observable<any>{
+    return this.client.get<any>(this.url);
   }
 }
