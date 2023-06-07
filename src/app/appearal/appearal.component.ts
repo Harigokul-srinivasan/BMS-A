5c2a1b36bbc52959c94bb95b27d185a8809205d4
Hari Gokul <ceeeharigokul23@gmail.com>
2023-06-07 11:36:28 +0530

    Hari Gokul

--- src/app/appearal/appearal.component.ts
+++ src/app/appearal/appearal.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../product.service';
 import {HttpClient} from '@angular/common/http'
 import { HomeServService } from '../home/homeServ.service';
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-appearal',
   templateUrl: './appearal.component.html',
@@ -12,12 +13,16 @@ export class AppearalComponent implements OnInit {
   allProduct: any = '';
   requiredProduct: any = '';
   finalProduct:any="";
-
+  public weddings:any;
+  title:any;
+  titleDescription:any;
+  p:number=1;
+  currentPage: number = 1;
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute,private http:HttpClient, private service:HomeServService
+    private route: ActivatedRoute,private http:HttpClient, private service:HomeServService, public services:ProductService,public cart:CartService,
   ) {
-    
+
   }
 
   sample:any=""
@@ -25,5 +30,43 @@ export class AppearalComponent implements OnInit {
     this.http.get<any>("http://localhost:3000/"+this.service.Store).subscribe(data=>{
       this.sample=data;
     })
+  // }
+  // ngOnInit():void{
+    this.http.get<any>("http://localhost:3000/"+this.service.Store).subscribe(data=>
+      {
+        this.weddings=data;
+        this.weddings.forEach((a:any)=>{
+          Object.assign(a,{quantity:1,total:a.price})
+      });
+    });
+    }
+
+    Search(){
+      if(this.title==""){
+        this.ngOnInit();
+      }
+      else{
+        this.weddings=this.weddings.filter((result: { title: string; }) =>{
+          return result.title.toLocaleLowerCase().match(this.title.toLocaleLowerCase());
+        });
+      }
+    }
+    loadData(){
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+    //on page change
+    onPageChange(page: number): void {
+      this.currentPage = page;
+      this.loadData();
+    }
+
+    //add to cart
+
+    addtocart(items:any){
+    this.cart.addtocart(items);
+    console.log(items)
+    }
+
+
   }
-}
+
