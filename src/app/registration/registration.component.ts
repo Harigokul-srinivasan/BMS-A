834f84ee5e64e93acdba701f0200f8fb800fe34a
Hari Gokul <ceeeharigokul23@gmail.com>
2023-08-01 11:32:28 +0530

    Hari Gokul

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -15,13 +15,13 @@ export class RegistrationComponent implements OnInit {
     this.route.navigate(['/login']);
   }
 
-  RegisterForm = new FormGroup({
-    firstname: new FormControl(),
-    Phoneno: new FormControl(),
-    email: new FormControl(),
-    password: new FormControl(),
-    confirm: new FormControl(),
-  });
+  // RegisterForm = new FormGroup({
+  //   firstname: new FormControl(),
+  //   Phoneno: new FormControl(),
+  //   email: new FormControl(),
+  //   password: new FormControl(),
+  //   confirm: new FormControl(),
+  // });
 
   constructor(
     private fb: FormBuilder,
@@ -31,13 +31,11 @@ export class RegistrationComponent implements OnInit {
 
   RegisterForms = this.fb.group(
     {
-      firstname: [, [Validators.required, Validators.pattern('^[a-zA-Z]+$')]],
-      Phoneno: [, [Validators.required,Validators.pattern('^[6-9]{1}[0-9]{9}$')]],
-      // mobilenumber:[,[Validators.required,Validators.pattern("^[6-9]{1}[0-9]{9}$")]],
-      email: [,[Validators.required, Validators.pattern('^([a-z0-9.-]+)@([a-z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})$')]],
+      firstname: [, [Validators.required, Validators.pattern('^(?!.*([a-zA-Z])\\1\\1)[a-zA-Z]+$')]],
+      Phoneno: [, [Validators.required,Validators.pattern('^[6-9](?!.*(\\d)\\1{4})\\d{9}$')]],
+      email: [,[Validators.required, Validators.pattern('^[a-zA-Z0-9._%+-]+@gmail\.com$')]],
       password: [, [Validators.required,Validators.pattern('^[A-Z]{1}[a-z]+[@/!/#/$/%/&][0-9]{2,4}$')]],
       confirm: [, [Validators.required,]],
-      // policy:[,[Validators.required]],
     },
     { validator: ConfirmedValidator('password', 'confirm') }
   );
